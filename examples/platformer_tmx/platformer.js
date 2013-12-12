4c98189ee6c4ba419aadfbf2109a5f3a86103ff9
picacure <jiangcheng.wxd@taobao.com>
2013-12-12 17:01:04 +0800

    beautify

--- examples/platformer_tmx/platformer.js
+++ examples/platformer_tmx/platformer.js
@@ -9,9 +9,9 @@
 // target for the player.
 window.addEventListener("load", function () {
 
-// Set up an instance of the Quintus engine  and include
-// the Sprites, Scenes, Input and 2D module. The 2D module
-// includes the `TileLayer` class as well as the `2d` componet.
+    // Set up an instance of the Quintus engine  and include
+    // the Sprites, Scenes, Input and 2D module. The 2D module
+    // includes the `TileLayer` class as well as the `2d` componet.
     var Q = window.Q = Quintus()
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
         // Maximize this game to whatever the size of the browser is
@@ -19,9 +19,9 @@ window.addEventListener("load", function () {
         // And turn on default input controls and touch input (for UI)
         .controls().touch()
 
-// ## Player Sprite
-// The very basic player sprite, this is just a normal sprite
-// using the player sprite sheet with default controls added to it.
+    // ## Player Sprite
+    // The very basic player sprite, this is just a normal sprite
+    // using the player sprite sheet with default controls added to it.
     Q.Sprite.extend("Player", {
 
         // the init constructor is called on creation
@@ -59,16 +59,16 @@ window.addEventListener("load", function () {
     });
 
 
-// ## Tower Sprite
-// Sprites can be simple, the Tower sprite just sets a custom sprite sheet
+    // ## Tower Sprite
+    // Sprites can be simple, the Tower sprite just sets a custom sprite sheet
     Q.Sprite.extend("Tower", {
         init: function (p) {
             this._super(p, { sheet: 'tower' });
         }
     });
 
-// ## Enemy Sprite
-// Create the Enemy class to add in some baddies
+    // ## Enemy Sprite
+    // Create the Enemy class to add in some baddies
     Q.Sprite.extend("Enemy", {
         init: function (p) {
             this._super(p, { sheet: 'enemy', vx: 100 });
@@ -97,8 +97,8 @@ window.addEventListener("load", function () {
         }
     });
 
-// ## Level1 scene
-// Create a new scene called level 1
+    // ## Level1 scene
+    // Create a new scene called level 1
     Q.scene("level1", function (stage) {
 
         // Add in a repeater for a little parallax action
@@ -125,9 +125,9 @@ window.addEventListener("load", function () {
         stage.insert(new Q.Tower({ x: 180, y: 50 }));
     });
 
-// To display a game over / game won popup box, 
-// create a endGame scene that takes in a `label` option
-// to control the displayed message.
+    // To display a game over / game won popup box,
+    // create a endGame scene that takes in a `label` option
+    // to control the displayed message.
     Q.scene('endGame', function (stage) {
         var container = stage.insert(new Q.UI.Container({
             x: Q.width / 2, y: Q.height / 2, fill: "rgba(0,0,0,0.5)"
@@ -149,10 +149,10 @@ window.addEventListener("load", function () {
         container.fit(20);
     });
 
-// ## Asset Loading and Game Launch
-// Q.load can be called at any time to load additional assets
-// assets that are already loaded will be skipped
-// The callback will be triggered when everything is loaded
+    // ## Asset Loading and Game Launch
+    // Q.load can be called at any time to load additional assets
+    // assets that are already loaded will be skipped
+    // The callback will be triggered when everything is loaded
     Q.load("sprites.png, sprites.json, level.tmx, tiles.png, background-wall.png", function () {
         // Sprites sheets can be created manually
         Q.sheet("tiles", "tiles.png", { tilew: 32, tileh: 32 });
@@ -164,15 +164,15 @@ window.addEventListener("load", function () {
         Q.stageScene("level1");
     });
 
-// ## Possible Experimentations:
-// 
-// The are lots of things to try out here.
-// 
-// 1. Modify level.json to change the level around and add in some more enemies.
-// 2. Add in a second level by creating a level2.json and a level2 scene that gets
-//    loaded after level 1 is complete.
-// 3. Add in a title screen
-// 4. Add in a hud and points for jumping on enemies.
-// 5. Add in a `Repeater` behind the TileLayer to create a paralax scrolling effect.
+    // ## Possible Experimentations:
+    //
+    // The are lots of things to try out here.
+    //
+    // 1. Modify level.json to change the level around and add in some more enemies.
+    // 2. Add in a second level by creating a level2.json and a level2 scene that gets
+    //    loaded after level 1 is complete.
+    // 3. Add in a title screen
+    // 4. Add in a hud and points for jumping on enemies.
+    // 5. Add in a `Repeater` behind the TileLayer to create a paralax scrolling effect.
 
 });
