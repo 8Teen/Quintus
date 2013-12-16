5460747166c0d930e6d76bedc3c8f1c020d55e60
picacure <jiangcheng.wxd@taobao.com>
2013-12-16 20:42:05 +0800

    beautify

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -2,35 +2,55 @@
  * Created by jiangcheng.wxd on 13-12-13.
  */
 
-window.addEventListener('load',function(){
+window.addEventListener('load', function () {
     // Set up a Quintus Instance
     var Q = window.Q = Quintus()
-        .include("Sprites, Scenes, Touch, UI")
+        .include("Sprites, Anim, Scenes, Touch, UI")
         .setup()
         .touch();
 
 
-    Q.scene("test", function (stage) {
-        var container = stage.insert(new Q.UI.Container({
-            w: "0%",
-            y: 50
-        }));
+    //Turtle.
+    Q.Sprite.extend("Turtle", {
+        init: function (p) {
+            this._super(p, {
+                sprite: "animado",
+                sheet: 'animado'
+            });
 
-        if (Q.touchDevice) {
-            stage.insert(new Q.UI.Text({
-                label: "HTML5 Audio\nnot well\nsupported\non Mobile",
-                align: "center",
-                y: 200
-            }), container);
+
+            this.add("animation");
         }
-
-        container.fit(20);
     });
 
-    Q.stageScene("test");
 
-    setTimeout(function(){
-        Q.clearStages();
-    },2000);
+    Q.scene("animate_tc", function (stage) {
+
+        //animation
+        var turtle = new Q.Turtle({ x: 100, y: 150 });
+
+        stage.insert(turtle);
+        turtle.play("expand");
+
+
+    });
+
+
+    //key 图片名称
+    Q.animations('animado', {
+        expand: { frames: [0, 1, 2, 3, 4, 5 , 6 , 7], rate: 1/3},
+        standAlone: { frames: [0], rate: 2, next: 'expand'}
+    });
+
+
+    Q.load([
+        'animado.gif', 'sprites.json'
+    ], function () {
+        // this will create the sprite sheets sprite1name and sprite2name
+
+        Q.compileSheets("animado.gif", "sprites.json");
+
+        Q.stageScene("animate_tc");
+    });
 
 });
